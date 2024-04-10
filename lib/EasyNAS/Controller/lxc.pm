f67ddebb195ebb605e193cc354f4788eebb3b9ac
Yariv Hakim <yariv.hakim@gmail.com>
2024-04-10 14:25:33 -0400

    Update lcx with system list

--- lib/EasyNAS/Controller/lxc.pm
+++ lib/EasyNAS/Controller/lxc.pm
@@ -1,18 +1,37 @@
 package EasyNAS::Controller::Lxc;
-use lib '.'; 
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use lib '/easynas/lib/EasyNAS/Controller';
 use Common;
-
 
 my $msg;
 my $result;
 my %systems;
+my @systems;
+my $name;
+my $state;
+my $ip;
 
+###### Get Systems List #######
+@systems=`/usr/bin/lxc-ls --fancy`;
+foreach (@systems) {
+ ($name,$state,undef,undef,$ip,undef)=split(" ",$_);
+ if ($name ne "NAME") {
+  $systems{$name}=[$name,$state,$ip];
+ }
+}
 
 sub view ($self) {
   $msg="";
   $result="";
-  $systems{test1} = ["test1","Stopped","192.168.1.40"];
+  my $action=$self->param('action'); 
+  
+  if (defined $action && $action eq "terminal" )
+  {
+    system("sudo /usr/bin/ttyd -p 8080 -o -W bash &");
+    $self->redirect_to('http://192.168.64.39:8080/'); 
+    return;
+  }  
+
   $self->render(template => 'easynas/lxc', 
 	        title => $TEXT{%addons{lxc}->{description}},
                 program => $addons{lxc}->{program},
