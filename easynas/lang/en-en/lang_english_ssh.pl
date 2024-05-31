b7c61156d9f491cdd93825d78d4ff2ac04303975
yarivh <yariv.hakim@gmail.com>
2024-05-31 10:57:19 -0400

    add ssh addon

---
+++ easynas/lang/en-en/lang_english_ssh.pl
@@ -0,0 +1,24 @@
+#
+# EasyNAS is free software: You can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# This file is part of @MYNAME_UC@ (c) created by @PROJECT_AUTHOR@ 2012-@YEAR@
+#
+# Homepage    : @PROJECT_URL@
+# Sourceforge : @PROJECT_SF_URL@
+#
+#########################################################################
+
+#### SVR_SSH ####
+$TEXT{'ssh'} = "SSH";
+$TEXT{'ssh_on_off'} = "ON/OFF";
+$TEXT{'ssh_settings'} = "Settings";
+$TEXT{'ssh_demo'} = "Can not disable ssh in a demo site";
+$TEXT{'ssh_not_installed'} = "SSH not installed";
---
+++ easynas/lib/EasyNAS/Controller/ssh.pm
@@ -0,0 +1,33 @@
+package EasyNAS::Controller::Ssh;
+use lib '/easynas/lib/EasyNAS/Controller';
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+use easynas;
+
+
+my $msg;
+my $result;
+my $addon = get_addon_info("ssh");
+
+my %TEXT=get_lang_text($addon->{'name'});
+
+sub view ($self) {
+  if (!($self->session('is_auth'))) {
+        $self->redirect_to('login');
+  }
+  my $action=$self->param('action'); 
+  $msg="";
+  $result="";
+  $self->stash(addon => $addon,
+                TEXT =>\%TEXT);
+
+
+##### menu ######
+
+  $self->stash(result => $result);
+  $self->stash(msg => $msg);
+  $self->render(template => 'easynas/ssh'); 
+
+}
+
+
+1;
